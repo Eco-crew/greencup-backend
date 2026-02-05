f83c0d284dc8494347f1b944d8057270f7ea4aa6
Joon <wushu80@hanmail.net>
2026-02-06 03:56:29 +0900

    feat(server): DB 커넥션 종료 등 서버 종료 이벤트 처리

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const express = require('express');
+const pool = require('./src/db/connection'); // MySQL Connection Pool
 const morgan = require('morgan');
-require('dotenv').config(); // 환경 변수 이용
+require('dotenv').config({ quiet: true }); // 환경 변수 이용
 
 // Router
 const authRoute = require('./src/auth/route');
@@ -25,3 +26,14 @@ app.use('/', partnerRoute);
 app.listen(PORT, () => {
   console.log(`Server is running on ${process.env.URL}:${PORT}`);
 });
+
+
+// 서버 종료 이벤트 처리
+const gracefulShutdown = async () => {
+  console.log('Shutting down DB connection pool...');
+  await pool.end(); // 커넥션 풀 내에 열려 있는 커넥션 모두 종료
+  process.exit(0);
+}
+
+process.on('SIGINT', gracefulShutdown); // Ctrl + C로 server.js 종료시
+process.on('SIGTERM', gracefulShutdown); // 배포/종료 시그널
