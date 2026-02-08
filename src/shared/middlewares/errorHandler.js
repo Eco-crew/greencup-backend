b1648fe95c75b0b9b112e58278fd33daf4f0ec68
Joon <wushu80@hanmail.net>
2026-02-09 04:52:08 +0900

    fix(auth): 에러 핸들러(미들웨어) 인수 수정
    - 에러 핸들러 함수 시그니처에서 4번째 인수로 next 함수 추가 (필수)

--- src/shared/middlewares/errorHandler.js
+++ src/shared/middlewares/errorHandler.js
@@ -1,4 +1,4 @@
-function errorHandler(err, req, res) {
+function errorHandler(err, req, res, next) {
   console.log(err);
 
   res.status(err.status || 500).json({ message: err.message || 'Server error' });
