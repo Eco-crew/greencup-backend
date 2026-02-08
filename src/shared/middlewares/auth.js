9cdcca787c8926fb1a1344d98d2739a6df68e412
Joon <wushu80@hanmail.net>
2026-02-09 04:55:22 +0900

    chore: 파일 역할 주석 추가

--- src/shared/middlewares/auth.js
+++ src/shared/middlewares/auth.js
@@ -1,4 +1,4 @@
-// Authentication(회원 인증), Authorization(권한 인가)
+// Authentication(회원 인증), Authorization(권한 인가) 처리 미들웨어
 function checkLogin(req, res, next) {
   // Guard clause (조건에 안 맞으면 조기 반환. "Fail fast, return early")
   if (!req.session.user) return res.status(401).json('message: 접근 권한이 없습니다');
--- src/shared/middlewares/errorHandler.js
+++ src/shared/middlewares/errorHandler.js
@@ -1,3 +1,4 @@
+// 공통 에러 처리 핸들러(미들웨어)
 function errorHandler(err, req, res, next) {
   console.log(err);
 
