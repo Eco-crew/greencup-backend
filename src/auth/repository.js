30d13f2736459dbd620eedcd9890dd688738c7e9
Joon <wushu80@hanmail.net>
2026-02-09 05:29:38 +0900

    fix(auth): DB 커넥션 함수 호출 로직 수정
    - profile() 등 다른 함수 스캐폴딩 추가
    - 주석 추가

--- src/auth/repository.js
+++ src/auth/repository.js
@@ -1,27 +1,35 @@
 // DB Connection Pool에서 가용 커넥션을 받아온다 (없으면 큐에서 요청 대기)
-const connection = require('../db/connection').getConnection();
+const db = require('../db/connection');
 
 
 /**********************
  *  OAuth 기반 로그인  *
  **********************/
 async function oauthLogin({ provider }) {
-
   // DB 쿼리 중 오류 발생시 오류는 Controller layer → Error Handler로 넘겨서 처리하므로 catch 블록 제외
   // 단, 리소스 반환 때문에 finally 구문은 사용
+  let connection;
+
   try {
+    connection = await db.getConnection();
+
     const user = await connection.execute();
-    res.json(user);
+    return user;
+
   } finally { // 반드시 처리해야 하는 마무리 작업은, 오류 발생시에도 '반드시 실행되는' finally 블록 안에서 처리 (try finally 없으면 실행 보장 불가)
     connection.release(); // DB 커넥션 사용 직후 커넥션풀에 커넥션을 반환함으로써 가용 리소스 확보 및 메모리 누수 방지
   }
 }
 
 async function callback({ provider }) {
+  let connection;
 
   try {
+    connection = await db.getConnection();
+
     const user = await connection.execute();
-    res.json(user);
+    return user;
+
   } finally {
     connection.release();
   }
@@ -34,8 +42,11 @@ async function callback({ provider }) {
 // function findUserByLoginId({ userId, userType }) {
 // 아직 프런트엔드와 조율되지 않아서, 기본값을 재사용 컵 사업자로 설정함으로써 인수가 없어도 실행되도록 했다.
 async function findUserByLoginId({ userId, userType = 'reuseOperator' }) {
+  let connection;
 
   try {
+    connection = await db.getConnection();
+
     const table = userType === 'reuseOperator' ? 'greencup_branches' : 'partners';
     const query = `
     SELECT id, manager_email, manager_name, password_hash
@@ -55,10 +66,21 @@ async function findUserByLoginId({ userId, userType = 'reuseOperator' }) {
 
 
 // My page
-async function profile({ username, password }) {
+async function profile({ userId, password }) {
+  // 인증 미들웨어 통과해서 왔는데도, 인수로 id, 비밀번호를 받아서 쿼리할 때 또 조건문에 넣어야 하나?
+  let connection;
 
   try {
-    const user = await connection.execute();
+    connection = await db.getConnection();
+
+    const table = userType === 'reuseOperator' ? 'greencup_branches' : 'partners';
+    const query = `
+    SELECT id, manager_email, manager_name, 기타 등등 (미완성)
+      FROM ${table}
+     WHERE manager_email = ? (미완성)
+    `;
+
+    const user = await connection.execute(query, userId);
     return user;
 
   } finally {
