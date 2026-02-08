5b37970ae02bb8e0b52628003c36d39ba18e5056
Joon <wushu80@hanmail.net>
2026-02-08 17:43:22 +0900

    feat(middleware): add login check middleware

--- src/shared/middlewares/auth.middleware.js
+++ src/shared/middlewares/auth.middleware.js
@@ -1,1 +1,8 @@
 // Authentication(회원 인증), Authorization(권한 인가)
+function checkLogin(req, res, next) {
+  if (req.session.user) return next();
+
+  res.status(403).json('message: 접근 권한이 없습니다');
+}
+
+module.exports = { checkLogin };
