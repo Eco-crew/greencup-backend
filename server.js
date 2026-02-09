20766870794703f2b29d588ff6dba96010b1d79a
Joon <wushu80@hanmail.net>
2026-02-10 04:05:41 +0900

    refactor(server): 서버와 앱 기능 분리
    - server.js: 서버 실행, 종료 작업만 전담
    - app.js: 패키지 로드, 미들웨어, 라우팅 처리

--- server.js
+++ server.js
@@ -1,37 +1,7 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const morgan = require('morgan');
-require('dotenv').config({ quiet: true }); // 환경 변수 이용
+const { app } = require('./app.js');
 const { pool } = require('./src/db/connection'); // MySQL Connection Pool
-const errorHandler = require('./src/shared/middlewares/errorHandler'); // Error Handler
-// Routers
-const authRoute = require('./src/auth/route');
-const reuseOperatorRoute = require('./src/reuse-operator/route');
-const partnerRoute = require('./src/partner/route');
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-
-// Middleware
-// app.use(express.static('public')); // frontend가 있어서 필요 없을 듯
-app.use(express.json());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false // `false` is useful for implementing login sessions, reducing server storage usage, or 생략
-}));
-app.use(morgan('dev'));
-
-// Middleware - Routing
-app.use('/', authRoute);
-// app.use('/', reuseOperatorRoute);
-// app.use('/', partnerRoute);
-
-// Middleware - Error Handler (※ 모든 라우터 등록 후 마지막에 등록해야 함!)
-app.use(errorHandler);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${process.env.URL}:${PORT}`);
