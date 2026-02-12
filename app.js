751b53e586c9fe50ff950b08e23354300d47cd64
Joon <wushu80@hanmail.net>
2026-02-12 17:59:38 +0900

    fix(route): 라우트 경로 변경

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use(session({
 app.use(morgan('dev'));
 
 // Middleware - Routing
-app.use('/', authRoute);
-// app.use('/', reuseOperatorRoute);
-// app.use('/', partnerRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/reuse-operator', reuseOperatorRoute);
+// app.use('/api/partner', partnerRoute);
 
 // Middleware - Error Handler (※ 모든 라우터 등록 후 마지막에 등록해야 함!)
 app.use(errorHandler);
