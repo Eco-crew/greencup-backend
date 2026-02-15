298bb7be45621987d390df44625f17b926f1bf36
Joon <wushu80@hanmail.net>
2026-02-16 08:10:26 +0900

    refactor(error): 에러 처리 시스템 개선
    - DBError 객체 추가
    - 공통 에러 처리 핸들러(미들웨어) 수정
    - 더 상세한 오류 로그 확인 가능

--- src/errors/DBError.js
+++ src/errors/DBError.js
@@ -1,12 +1,14 @@
 const AppError = require('./AppError');
 
-// 인증 에러 클래스
-class AuthError extends AppError {
-  constructor(message = '인증 실패', status = 401) {
+// DB 에러 클래스
+class DBError extends AppError {
+  constructor(message, query = null, params = null, status = 500) {
     super(message, status);
+    this.query = query; // 에러 발생 쿼리
+    this.params = params; // Binding Parameter (예: [id, status])
     this.name = this.constructor.name; // 클래스 이름 저장
     // 객체를 생성해서 err라는 변수에 바인딩했다고 가정하면, err.constructor.name 은 'AppError'가 된다 → 로그 출력할 때 에러 종류 식별 가능
   }
 }
 
-module.exports = AuthError;
+module.exports = DBError;
--- src/shared/middlewares/errorHandler.js
+++ src/shared/middlewares/errorHandler.js
@@ -1,16 +1,34 @@
 const AuthError = require('../../errors/AuthError');
+const DBError = require('../../errors/DBError');
 
 // 공통 에러 처리 핸들러(미들웨어)
 // ※ errorHandler Middleware로 등록해서 사용하려면 함수 시그니쳐가 반드시 (err, req, res, next)이어야 한다.
 // 그래야 Express server가 에러 처리 미들웨어로 인식한다. errorHandler 함수 안에서 next 등을 사용하지 않더라도 마찬가지다.
 function errorHandler(err, req, res, next) {
-  console.log(err);
+
+  const status = err.status || 500;
+  const errorObject = {
+    status,
+    error: err.name,
+    message: err.message || 'Internal Server error',
+  };
 
   if (err instanceof AuthError) {
-    return res.status(err.status).json({ isLoggedIn: false, message: err.message });
+    errorObject.isLoggedIn = false;
+  } else if (err instanceof DBError) {
+    console.log('[DBError] query:', err.query); // 보안 때문에 SQL Query 정보는 백엔드에서만 로그로 확인
+    console.log('[DBError] params:', err.params); // Binding Parameter (예: [id, status])
   }
 
-  res.status(err.status || 500).json({ message: err.message || 'Internal Server error' });
+  console.log('errorObject to send to FE:', errorObject);
+
+  res.status(status).json(errorObject);
+
+  // if (err instanceof AuthError) {
+  //   return res.status(status).json({ status, message: err.message || 'Internal Server error', isLoggedIn: false });
+  // }
+
+  // res.status(status).json({ status, message: err.message || 'Internal Server error' });
 }
 
 module.exports = errorHandler;
